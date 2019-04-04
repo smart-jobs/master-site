30edcccbd3bd6fec27c330bcb77be12dc77a0ee2
dygapp <dygapp@qq.com>
2019-04-04 10:06:32 +0800

    增加浏览器版本检查

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <div class="alert" style="display: none;" v-show="showAlert">
+      <span>您使用的浏览器版本过低，可能会影响网站浏览效果，建议使用IE10以上浏览器或最新版谷歌浏览器。</span>
+    </div>
     <div class="header">
       <banner />
       <navx />
@@ -17,12 +20,30 @@
 import banner from '~/components/layout/banner';
 import navx from '~/components/layout/navx';
 import feet from '~/components/layout/feet';
+import { detect } from 'detect-browser';
+import semver from 'semver';
 
 export default {
   components: {
     banner,
     navx,
     feet,
+  },
+  data() {
+    return {
+      showAlert: false,
+    };
+  },
+  mounted() {
+    // 检查浏览器版本
+    if (process.browser) {
+      const browser = detect();
+      console.log(browser);
+      if(browser.name === 'ie' && semver.lt(browser.version, '10.0.0')) {
+        this.showAlert = true;
+        console.warn('您使用的浏览器版本过低，可能会影响网站浏览效果，请更换新版本的浏览器，建议使用最新版谷歌浏览器。')
+      }
+    }
   },
 };
 </script>
@@ -81,5 +102,18 @@ html {
 }
 .content > div {
   min-height: 400px;
+}
+
+.alert {
+  text-align: center;
+  line-height: 24px;
+  background: lightyellow;
+  position: fixed;
+  top: 0;
+  z-index: 99999;
+  width: 100%;  
+}
+.alert span {
+  display: inline;
 }
 </style>
